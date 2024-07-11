22416e8e75222d67a650c60c02793654010bf60d
Rida Labiad <115871096+whitejames1234@users.noreply.github.com>
2024-07-11 09:59:41 +0200

    version1

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-[30px] text-center text-gray-50 px-4 py-4 bg-green-600 uppercase	font-semibold ">
+      hello world
+    </div>
+  )
+}
+
+export default App
